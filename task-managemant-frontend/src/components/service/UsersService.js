55ec3411db1ac50ce9496b42994ad01b6c3f1770
Dilshan012 <dilnet.it@gmail.com>
2024-06-14 09:47:18 +0530

    Update 1.1

--- task-managemant-frontend/src/components/service/UsersService.js
+++ task-managemant-frontend/src/components/service/UsersService.js
@@ -30,9 +30,9 @@ class UsersService {
     }
 
 
-    static async getAllUsers(userData, token) {
+    static async getAllUsers(token) {
         try{
-            const response = await axios.post(`${UserService.BASE_URL}/admin/get-all-users`, userData, {
+            const response = await axios.get(`${UserService.BASE_URL}/admin/get-all-users`,{
                 headers: {
                     Authorization: `Bearer ${token}`
                 }
@@ -45,5 +45,73 @@ class UsersService {
 
     }
 
+    static async getYourProfile(token) {
+        try{
+            const response = await axios.get(`${UserService.BASE_URL}/adminuser/get-profile`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        }
+        catch(err){
+            throw err;
+        }
 
+    }
+
+    static async getUserById(userId, token) {
+        try{
+            const response = await axios.get(`${UserService.BASE_URL}/admin/get-user/${userId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        }
+        catch(err){
+            throw err;
+        }
+
+    }
+
+    static async deleteUser(userId, token) {
+        try{
+            const response = await axios.delete(`${UserService.BASE_URL}/admin/delete/${userId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        }
+        catch(err){
+            throw err;
+        }
+
+    }
+
+    static async updateUser(userId, userData, token) {
+        try{
+            const response = await axios.delete(`${UserService.BASE_URL}/admin/update/${userId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response.data;
+        }
+        catch(err){
+            throw err;
+        }
+
+    }
+
+    /**AUTHENTIFICATION CHECKER */
+    static logout(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('role')
+    }
+
+    static isAuthenticated(){
+        return localStorage.getItem('token') !== null
+    } 
 }
